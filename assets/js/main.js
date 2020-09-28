65f8cc223786b074508262a95782577f109df6f7
RahulChandaTV <rahulchanda.tv@gmail.com>
2020-09-28 12:57:39 +0530

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+     
+    var twitSlide = new Swiper('#twitSlide', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+        }
+      });
+
+});
+
+
